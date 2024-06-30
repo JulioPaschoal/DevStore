2afa20b11d2fcad2e6030d833b3d4bac06c78473
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2024-06-30 11:53:47 -0300

    Configurando variavel production

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -11,7 +11,6 @@ async function getFeaturedProducts(): Promise<Product[]> {
 
 export default async function Home() {
   const [highlightedProduct, ...otherProducts] = await getFeaturedProducts()
-
   return (
     <div className="grid max-h[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
@@ -26,7 +25,6 @@ export default async function Home() {
           quality={100}
           alt=""
         />
-
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-5">
           <span className="text-sm truncate">{highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
@@ -39,7 +37,6 @@ export default async function Home() {
           </span>
         </div>
       </Link>
-
       {otherProducts.map((product) => {
         return (
           <Link
