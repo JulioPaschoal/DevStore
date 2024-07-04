d446ef61a69f4d4b5edeac7abb4f240a4d2646bd
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2024-07-04 08:54:38 -0300

    Estilizando pagina de produto

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -18,7 +18,7 @@ export default async function Home() {
   return (
     <div className="grid max-h[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href={`/products/${highlightedProduct.slug}`}
+        href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
@@ -45,7 +45,7 @@ export default async function Home() {
         return (
           <Link
             key={product.id}
-            href={`/products/${product.id}`}
+            href={`/product/${product.id}`}
             className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
           >
             <Image
