4c66b4e5ae2662414c6a56f21a634cb88cc8ca67
JULIOPASCHOAL <juliocpaschoal@gmail.com>
2024-07-08 08:54:38 -0300

    Criando pagina de produtos

--- src/app/(store)/product/[slug]/page.tsx
+++ src/app/(store)/product/[slug]/page.tsx
@@ -12,6 +12,20 @@ export default function ProductPage() {
           quality={100}
         />
       </div>
+      <div className="flex flex-col justify-center px-12">
+        <h1 className="text-3xl font-bold leading-tight">
+          Moletom Never Stop Learning
+        </h1>
+        <p className="mt-2 leading-relaxed text-zinc-400">
+          Moletom fabricado com 88% de algodão e 12% de poliéster.
+        </p>
+        <div className="mt-8 flex items-center- gap-3">
+          <span className="inline-block rounded-full bg-violet-500 px-5 py-2.5">
+            R$129
+          </span>
+          <span>Em 12x s/ juros de R$10,75</span>
+        </div>
+      </div>
     </div>
   )
 }
